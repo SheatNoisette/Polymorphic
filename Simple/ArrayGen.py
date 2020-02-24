767303f0eb7698a4ac04f7792b5406c439588c84
SheatNoisette <SheatNoisette@protonmail.com>
2020-02-25 01:12:58 +0530

    Reseted the key in simple and corrected a typo

--- Simple/ArrayGen.py
+++ Simple/ArrayGen.py
@@ -1,5 +1,7 @@
+""" Generate the encoded string """
+
 program = """
-print("I'm a evil program! I can cypher myself again when you run me.")
+print("I'm an evil program! I can cypher myself again when you run me.")
 """
 
 def generateArray(string):
